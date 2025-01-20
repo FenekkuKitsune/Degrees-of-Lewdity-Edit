44a6c9f77251f393cab1d8941a5e25f3422d88f6
FenekkuKitsune <135238109+FenekkuKitsune@users.noreply.github.com>
2025-01-21 00:09:00 +1300

    Reduced code repetition, moved funcs for object oriented programming
    Reduced code repetition by replacing all the setAttribute() calls with a singular function to do it all at once. In addition, moved the functions from the bottom of the code to the DoLE to keep things all in one place, and because I really like object oriented programming.

--- usettings.js
+++ usettings.js
@@ -9,48 +9,135 @@ const DoLE = {
 	},
 	"inputs": {
 		"debt":null
+	},
+	"newElement":function(type, attrs, appendTo, text=null){
+		let el = document.createElement(type);
+	
+	
+		for(let attr in attrs){
+			el.setAttribute(attr, attrs[attr]);
+		}
+	
+		appendTo.appendChild(el);
+	
+		if(text!=null){
+			el.appendChild(document.createTextNode(text));
+		}
+	
+		return el;
+	},
+	"openMenu":function(){
+		// Reveal the overlay
+		DoLE.el.menu.classList.remove("hidden");
+	
+		// Change the function on the button to close instead of open
+		DoLE.el.toggle.setAttribute("onclick","DoLE.closeMenu()");
+	
+		// Highlight the button
+		DoLE.el.toggle.classList.add("macro-button-selected");
+	},
+	"closeMenu":function(){
+		// Hide the overlay
+		DoLE.el.menu.classList.add("hidden");
+	
+		// Set the button back to open
+		DoLE.el.toggle.setAttribute("onclick","DoLE.openMenu()");
+	
+		// Remove the button highlight
+		DoLE.el.toggle.classList.remove("macro-button-selected");
+	},
+	"switchTab":function(tab){
+		let tabs = DoLE.el.tab;
+		let contents = DoLE.el.content;
+	
+		for(let i=0; i<tabs.length; i++){
+			if(i!==tab){
+				tabs[i].classList.remove("tab-selected");
+				contents[i].setAttribute("class","hidden");
+			} else{
+				tabs[i].classList.add("tab-selected");
+				contents[i].setAttribute("class","");
+			}
+		}
+	},
+	"setDebt":function(){
+		let val = DoLE.inputs.debt.value*100;
+		let debt = SugarCube.State.variables.rentmoney;
+	
+		// Set the value to the input if a value was provided, otherwise just invert the debt.
+		if(val!=0){
+			SugarCube.State.variables.rentmoney=val;
+		} else {
+			SugarCube.State.variables.rentmoney=-Math.abs(SugarCube.State.variables.rentmoney);
+		}
+		
+		alert("Bailey Debt: £"+(debt*0.01)+" is now £"+(SugarCube.State.variables.rentmoney*0.01));
+	},
+	"cleanBody":function(){
+		// let bl = SugarCube.State.variables.player.bodyliquid;
+	
+		// Set all body liquids to 0, which cleans the player of all external liquids.
+		for(let area in SugarCube.State.variables.player.bodyliquid){
+			// Set each value individually. This is purely so we can avoid issues if future game updates add other liquids.
+			SugarCube.State.variables.player.bodyliquid[area].goo=0;
+			SugarCube.State.variables.player.bodyliquid[area].semen=0;
+			SugarCube.State.variables.player.bodyliquid[area].nectar=0;
+		}
+		
+		alert("Body cleaned of all external liquids");
 	}
 };
 
 // Create button, copy base game UI, but position it in the bottom right.
-DoLE.el.toggle = document.createElement("button");
-DoLE.el.toggle.setAttribute("class","link-internal macro-button");
-DoLE.el.toggle.setAttribute("style","position:fixed; right:0em; bottom:0em; max-width:initial; width:5em;");
-DoLE.el.toggle.setAttribute("type","button");
-DoLE.el.toggle.setAttribute("role","button");
-DoLE.el.toggle.setAttribute("tabindex","0");
-DoLE.el.toggle.setAttribute("onclick","DoLOpenMenu()");
-DoLE.el.toggle.appendChild(document.createTextNode("DoLEdit"));
-document.body.appendChild(DoLE.el.toggle);
+DoLE.el.toggle = DoLE.newElement(
+	"button",
+	{
+		"class":"link-internal macro-button",
+		"style":"position:fixed; right:0em; bottom:0em; max-width:initial; width:5em;",
+		"type":"button",
+		"role":"button",
+		"tabindex":"0",
+		"onclick":"DoLE.openMenu()"
+	},
+	document.body,
+	"DoLEdit"
+);
 
 // Create menu
-DoLE.el.menu = document.createElement("div");
-DoLE.el.menu.setAttribute("style","display:block; position:fixed; right:6em; bottom:0em; width:50em; height:20em; line-height:1.5em; text-align:left;");
-DoLE.el.menu.setAttribute("class","hidden");
-document.body.appendChild(DoLE.el.menu);
+DoLE.el.menu = DoLE.newElement(
+	"div",
+	{
+		"style":"display:block; position: fixed; right:6em; bottom:0em; width:50em; height:20em; line-height:1.5em; text-align:left;",
+		"class":"hidden"
+	},
+	document.body
+);
 
 // Title bar
-let DoLETitleBar = document.createElement("div");
-DoLETitleBar.setAttribute("style","display:flex; flex-wrap:wrap; align-items:center; padding:0.5rem; padding-bottom:0;");
-DoLE.el.menu.appendChild(DoLETitleBar);
+let DoLETitleBar = DoLE.newElement("div", {"style":"display:flex; flex-wrap:wrap; align-items:center; padding:0.5rem; padding-bottom:0;"}, DoLE.el.menu);
 
 // Tab list
-DoLE.el.tablist = document.createElement("div");
-DoLE.el.tablist.setAttribute("class","tab");
-DoLETitleBar.appendChild(DoLE.el.tablist);
+DoLE.el.tablist = DoLE.newElement("div", {"class":"tab"}, DoLETitleBar);
 
 // Tabs
 for(let i=0; i<4; i++){
-	DoLE.el.tab.push(document.createElement("button"));
-	DoLE.el.tab[i].setAttribute("class","link-internal macro-button");
-	DoLE.el.tab[i].setAttribute("type","button");
-	DoLE.el.tab[i].setAttribute("role","button");
-	DoLE.el.tab[i].setAttribute("tabindex","0");
-	DoLE.el.tab[i].setAttribute("onclick","DoLSwitchTab("+i+")");
-	DoLE.el.tablist.appendChild(DoLE.el.tab[i]);
+	DoLE.el.tab.push(DoLE.newElement(
+		"button",
+		{
+			"class":"link-internal macro-button",
+			"type":"button",
+			"role":"button",
+			"tabindex":"0",
+			"onclick":"DoLE.switchTab("+i+")"
+		},
+		DoLE.el.tablist
+	));
+
+	// First tab is always selected initially
+	if(i===0){
+		DoLE.el.tab[i].classList.add("tab-selected");
+	}
 }
-// First tab is always selected initially
-DoLE.el.tab[0].classList.add("tab-selected");
 
 // Tab names
 DoLE.el.tab[0].appendChild(document.createTextNode("Game"));
@@ -59,144 +146,82 @@ DoLE.el.tab[2].appendChild(document.createTextNode("Body"));
 DoLE.el.tab[3].appendChild(document.createTextNode("Social"));
 
 // Close button
-let DoLEClose = document.createElement("div");
-DoLEClose.setAttribute("class","customOverlayClose");
-DoLEClose.setAttribute("onclick","DoLCloseMenu()");
-DoLEClose.setAttribute("style","top:0.8rem");
-DoLEClose.setAttribute("onclick","DoLCloseMenu()");
-DoLE.el.tablist.appendChild(DoLEClose);
+let DoLEClose = DoLE.newElement(
+	"div",
+	{
+		"class":"customOverlayClose",
+		"style":"top:0.8rem",
+		"onclick":"DoLE.closeMenu()"
+	},
+	DoLE.el.tablist
+);
 
 // Content
 for(let i=0; i<4; i++){
-	DoLE.el.content[i] = document.createElement("div");
-	DoLE.el.content[i].classList.add("hidden");
-	DoLE.el.content[i].setAttribute("style","position:relative; padding:0.5rem; margin:0.5em; margin-top:0; z-index:0; border:1px solid var(--150); border-top:0; background-color:var(--850); overflow-y:scroll; height:calc(100% - 60px);");
-	DoLE.el.menu.appendChild(DoLE.el.content[i]);
+	DoLE.el.content.push(DoLE.newElement(
+		"div",
+		{"style":"position:relative; padding:0.5rem; margin:0.5em; margin-top:0; z-index:0; border:1px solid var(--150); border-top:0; background-color:var(--850); overflow-y:scroll; height:calc(100% - 60px);"},
+		DoLE.el.menu
+	));
+	// First tab is always selected initially
+	if(i!==0){
+		DoLE.el.content[i].classList.add("hidden");
+	}
 }
-// First tab is always selected initially
-DoLE.el.content[0].classList.remove("hidden");
-
 // Tab 1
-let DoLETable = document.createElement("table");
-DoLETable.setAttribute("style","border-top:1px solid white; border-right:1px solid white; width:100%;");
-DoLE.el.content[0].appendChild(DoLETable);
+let DoLETable = DoLE.newElement("table", {"style":"border-top:1px solid white; border-right:1px solid white; width:100%;"}, DoLE.el.content[0]);
 
-let DoLETBody = document.createElement("tbody");
-DoLETable.appendChild(DoLETBody);
+let DoLETBody = DoLE.newElement("tbody", {}, DoLETable);
 
-let DoLETR = document.createElement("tr");
-DoLETR.setAttribute("style","border-bottom:1px solid white;");
-DoLETBody.appendChild(DoLETR);
+let DoLETR = DoLE.newElement("tr", {"style":"border-bottom:1px solid white;"}, DoLETBody);
 
 // Bailey's debt
-let DoLETD = document.createElement("td");
-DoLETD.setAttribute("style","border-left:1px solid white; width:40%;");
-DoLETR.appendChild(DoLETD);
+let DoLETD = DoLE.newElement("td", {"style":"border-left:1px solid white; width:40%"}, DoLETR);
 
-DoLE.inputs.debt = document.createElement("input");
-DoLE.inputs.debt.setAttribute("name","DoLEDebt");
-DoLE.inputs.debt.setAttribute("type","text");
-DoLE.inputs.debt.setAttribute("inputmode","text");
-DoLE.inputs.debt.setAttribute("tabindex","0");
-DoLE.inputs.debt.setAttribute("class","macro-textbox");
-DoLE.inputs.debt.setAttribute("style","width:5%; min-width:3em;");
-DoLE.inputs.debt.setAttribute("placeholder",-Math.abs(SugarCube.State.variables.rentmoney*0.01));
-DoLETD.appendChild(DoLE.inputs.debt);
+DoLE.inputs.debt = DoLE.newElement(
+	"input",
+	{
+		"name":"DoLEDebt",
+		"type":"text",
+		"inputmode":"text",
+		"tabindex":"0",
+		"class":"macro-textbox",
+		"style":"width:5%; min-width:3em",
+		"placeholder":-Math.abs(SugarCube.State.variables.rentmoney*0.01)
+	},
+	DoLETD
+);
 
-let DoLEButton = document.createElement("button");
-DoLEButton.setAttribute("style","padding:0.3em;");
-DoLEButton.setAttribute("onclick","DoLSetDebt()");
-DoLETD.appendChild(DoLEButton);
-DoLEButton.appendChild(document.createTextNode("Debt"));
+let DoLEButton = DoLE.newElement(
+	"button",
+	{
+		"style":"padding:0.3em;",
+		"onclick":"DoLE.setDebt()"
+	},
+	DoLETD,
+	"Debt"
+);
 
 // Tab 2
 
 
 // Tab 3
-DoLETable = document.createElement("table");
-DoLETable.setAttribute("style","border-top:1px solid white; border-right:1px solid white; width:100%;");
-DoLE.el.content[2].appendChild(DoLETable);
+DoLETable = DoLE.newElement("table", {"style":"border-top:1px solid white; border-right:1px solid white; width:100%;"}, DoLE.el.content[2]);
 
-DoLETBody = document.createElement("tbody");
-DoLETable.appendChild(DoLETBody);
+DoLETBody = DoLE.newElement("tbody", {}, DoLETable);
 
-DoLETR = document.createElement("tr");
-DoLETR.setAttribute("style","border-bottom:1px solid white");
-DoLETBody.appendChild(DoLETR);
+DoLETR = DoLE.newElement("tr", {"style":"border-bottom:1px solid white"}, DoLETBody);
 
 // Clean Body
-DoLETD = document.createElement("td");
-DoLETD.setAttribute("style","border-left:1px solid white; width:40%;");
-DoLETR.appendChild(DoLETD);
+DoLETD = DoLE.newElement("td", {"style":"border-left:1px solid white; width:40%;"}, DoLETR);
 
 // In Future: Specify levels to set body clean states
-DoLEButton = document.createElement("button");
-DoLEButton.setAttribute("style","padding:0.3em;");
-DoLEButton.setAttribute("onclick","DoLCleanBody()");
-DoLETD.appendChild(DoLEButton);
-DoLEButton.appendChild(document.createTextNode("Clean Body"));
-
-function DoLOpenMenu(){
-	// Reveal the overlay
-	DoLE.el.menu.classList.remove("hidden");
-
-	// Change the function on the button to close instead of open
-	DoLE.el.toggle.setAttribute("onclick","DoLCloseMenu()");
-
-	// Highlight the button
-	DoLE.el.toggle.classList.add("macro-button-selected");
-}
-
-function DoLCloseMenu(){
-	// Hide the overlay
-	DoLE.el.menu.classList.add("hidden");
-
-	// Set the button back to open
-	DoLE.el.toggle.setAttribute("onclick","DoLOpenMenu()");
-
-	// Remove the button highlight
-	DoLE.el.toggle.classList.remove("macro-button-selected");
-}
-
-function DoLSwitchTab(tab){
-	let tabs = DoLE.el.tab;
-	let contents = DoLE.el.content;
-
-	for(let i=0; i<tabs.length; i++){
-		if(i!==tab){
-			tabs[i].classList.remove("tab-selected");
-			contents[i].setAttribute("class","hidden");
-		} else{
-			tabs[i].classList.add("tab-selected");
-			contents[i].setAttribute("class","");
-		}
-	}
-}
-
-function DoLSetDebt(){
-	let val = DoLE.inputs.debt.value*100;
-	let debt = SugarCube.State.variables.rentmoney;
-
-	// Set the value to the input if a value was provided, otherwise just invert the debt.
-	if(val!=0){
-		SugarCube.State.variables.rentmoney=val;
-	} else {
-		SugarCube.State.variables.rentmoney=-Math.abs(SugarCube.State.variables.rentmoney);
-	}
-	
-	alert("Bailey Debt: £"+(debt*0.01)+" is now £"+(SugarCube.State.variables.rentmoney*0.01));
-}
-
-function DoLCleanBody(){
-	// let bl = SugarCube.State.variables.player.bodyliquid;
-
-	// Set all body liquids to 0, which cleans the player of all external liquids.
-	for(let area in SugarCube.State.variables.player.bodyliquid){
-		// Set each value individually. This is purely so we can avoid issues if future game updates add other liquids.
-		SugarCube.State.variables.player.bodyliquid[area].goo=0;
-		SugarCube.State.variables.player.bodyliquid[area].semen=0;
-		SugarCube.State.variables.player.bodyliquid[area].nectar=0;
-	}
-	
-	alert("Body cleaned of all external liquids");
-}
+DoLEButton = DoLE.newElement(
+	"button",
+	{
+		"style":"padding:0.3em",
+		"onclick":"DoLE.cleanBody()"
+	},
+	DoLETD,
+	"Clean Body"
+);
